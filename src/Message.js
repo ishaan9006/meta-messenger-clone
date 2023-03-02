38a2818075bc79aa0983e6b7398042197a5a9ff1
Ishaan Khullar <ishaan.khullar06@gmail.com>
2023-03-02 16:01:39 +0530

    Added structure!

--- src/Message.js
+++ src/Message.js
@@ -1,11 +1,27 @@
+import { Card, CardContent, Typography } from '@material-ui/core'
 import React from 'react'
+import './Message.css';
 
-function Message() {
-  return (
-    <div>
-      
-    </div>
-  )
+
+function Message({username, message}) {
+    const isUser = username == message.username;
+
+    return (
+        <div className={`message ${isUser && 'message__user'}`}>
+            <Card className={isUser ? "message_userCard" : "message_questCard"}>
+                <CardContent>
+                    <Typography
+                        color='white'
+                        variant='h5'
+                        component='h2'
+                    >
+                        {message.username}: {message.message}
+                    </Typography>
+                </CardContent>
+            </Card>
+        </div>
+
+    )
 }
 
 export default Message
